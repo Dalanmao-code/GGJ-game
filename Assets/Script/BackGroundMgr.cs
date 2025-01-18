b92f7794f34bf4493a47b6c1e451b293da7ce31d
fsrshaorong <53031222+fsrshaorong@users.noreply.github.com>
2025-01-18 19:41:29 +0800

    更新进度 场景切换等

--- Assets/Script/BackGroundMgr.cs
+++ Assets/Script/BackGroundMgr.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum BackGroundType
+{
+    living,
+    sick,
+    exhibt,
+    work,
+}
+
 
 public class BackGroundMgr:Singleton<BackGroundMgr>
 {
     private BackGround _backGround;
     
+    private void Start()
+    {
+        _backGround = GetComponent<BackGround>();
+    }
+
     /// <summary>
     /// 切换场景时调用
     /// </summary>
-    public void ChangeBackGround()
+    public void ChangeBackGround(BackGroundType bt)
     {
-
+        _backGround.ChangeBackGround(bt);
     }
 }
---
+++ Assets/Script/Iinteract.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 可交互物体接口 需要有碰撞体组件
+/// </summary>
+public interface Iinteract
+{
+    public void InteractEvent();
+}
